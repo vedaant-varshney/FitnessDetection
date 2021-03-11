98d89df66ca0d14244bbaa006da0a011a3b0ae06
Deep-Thoughts42 <53351935+Deep-Thoughts42@users.noreply.github.com>
2021-03-10 20:41:37 -0500

    Processed all sets to be compatible with deeplab

--- main_project/vedaant/resize_all.py
+++ main_project/vedaant/resize_all.py
@@ -1,6 +1,24 @@
 # This is a placeholder file that will serve the purpose of resizing all of the images within each set
 
 import os
+import cv2
+import numpy
+
+curDir = os.getcwd()
+
+set = "Set1"
+
+path = os.path.join(curDir, "images", set, '1.png' )
+
+img = cv2.imread(path)
+
+cv2.imshow("Test",img )
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+dim = (100, 100)
+
 
 
 # Assumes cwd is root
